ab98460a92c3cf1d6f1d7c4e7edad535b6c74daa
esahulkko <esa.hulkko@teliacompany.com>
2019-02-26 11:39:48 +0200

    gcloud test1

---
+++ gpc_creds.sh
@@ -0,0 +1,5 @@
+export GCP_PROJECT='ansible-demo'
+export GCP_AUTH_KIND='serviceaccount'
+export GCP_SERVICE_ACCOUNT_FILE='/myaccount.json'
+export GCP_SCOPES='https://www.googleapis.com/auth/compute'
+
